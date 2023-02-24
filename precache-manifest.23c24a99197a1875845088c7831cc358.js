7ba014ea41c7751721695f6e2dea29c5b94e47a1
vincent-perez <vincent.perez@knights.ucf.edu>
2023-02-24 16:51:04 -0500

    Updates

---
+++ precache-manifest.23c24a99197a1875845088c7831cc358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b7ea47c0076d4ac170a",
+    "url": "/exercise-app-demo/static/css/main.61f8a07e.chunk.css"
+  },
+  {
+    "revision": "2b7ea47c0076d4ac170a",
+    "url": "/exercise-app-demo/static/js/main.2b7ea47c.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/exercise-app-demo/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/exercise-app-demo/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "d28aac64a0daf720ac64",
+    "url": "/exercise-app-demo/static/js/runtime~main.d28aac64.js"
+  },
+  {
+    "revision": "69f81ff63db4e3fa0d3cea87883d8d46",
+    "url": "/exercise-app-demo/index.html"
+  }
+];
